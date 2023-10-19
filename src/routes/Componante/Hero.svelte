a82348e496f03ea924979264d5d91a9b781fdf95
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-10-19 11:52:35 +0200

    based + hero

---
+++ src/routes/Componante/Hero.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+<section>
+    <h1>Team PinPin</h1>
+    <p>“Les deux arts d’écrire et de dessiner
+         sont naturellement associés.” (Charles Dickens)</p>
+
+         <img src="/images/line/line2.svg" alt="line">
+</section>
+
+
+<style lang="scss">
+    section{
+        width: 100vw;
+        height: 90vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+
+    }
+    p{
+            width:40vw; 
+            text-align: center;
+        }
+
+    img{
+        position: absolute;
+        transform: translateY(18vw);
+        width: 100vw;
+    }
+
+</style>
