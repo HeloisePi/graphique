75f27c94adadef9d8b858feff10f1b5e44d9c669
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-11-17 20:30:00 +0100

    Suppr build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-//import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-auto";
 
-export default {
-	kit: {
-		adapter: adapter({
-			// will create a Netlify Edge Function using Deno-based
-			// rather than using standard Node-based functions
-			edge: true
-		})
-	}
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+  },
+
+  preprocess: [vitePreprocess({})],
+};
+
+export default config;
