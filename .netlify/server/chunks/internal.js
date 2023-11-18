a23212971735c497fa3e17d1e98b2a7b21f116a3
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-11-18 14:48:55 +0100

    I try

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -178,7 +178,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "5bzqqj"
+  version_hash: "1vz3mwl"
 };
 function get_hooks() {
   return {};
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a9976dc9.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.9523c766.js"];
+export const imports = ["_app/immutable/nodes/1.fcb5c6b3.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.1741ea0b.js"];
 export const stylesheets = [];
 export const fonts = [];
