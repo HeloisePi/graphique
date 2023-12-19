54eceba5dde09178f1e5fb6d41e4279900e0a497
Héloïse P <heloise.pingitore@mmibordeaux.com>
2023-12-19 12:46:33 +0100

    Add title Icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script>
 	import Contact from "./Componante/Contact.svelte";
 	import CreationSerge from "./Componante/CreationSerge.svelte";
-	import Hero from "./Componante/Hero.svelte";
-	
+	import Hero from "./Componante/Hero.svelte";	
 	import Saga from "./Componante/Saga.svelte";
 
 </script>
 
 <svelte:head>
-	<title>Team PinPin</title>
-	<meta name="description" content="“Les deux arts d’écrire et de dessiner
-	sont naturellement associés.” (Charles Dickens)" />
+	<title>L'épopée Graphique</title>
+	<meta name="description" content="'Les deux arts d’écrire et de dessiner
+	sont naturellement associés.' (Charles Dickens)" />
 </svelte:head>
 
 <section>
